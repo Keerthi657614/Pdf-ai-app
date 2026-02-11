ebfcc18f0ca5ecfca2c7441172f4a7a140bcf9d3
Keerthi657614 <147122327+Keerthi657614@users.noreply.github.com>
2026-02-11 23:04:16 +0530

    Modify PDF.js worker configuration
    Updated PDF.js worker configuration for Vite compatibility.

--- smartdocs-frontend/src/components/PdfViewer.jsx
+++ smartdocs-frontend/src/components/PdfViewer.jsx
@@ -5,7 +5,12 @@ import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 
 // Worker configuration
-pdfjs.GlobalWorkerOptions.workerSrc = "/pdfjs/pdf.worker.min.mjs";
+//pdfjs.GlobalWorkerOptions.workerSrc = "/pdfjs/pdf.worker.min.mjs";
+// Worker configuration (Vite compatible)
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.mjs",
+  import.meta.url
+).toString();
 
 export default function PdfViewer({ file, page }) {
   if (!file) {
